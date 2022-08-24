62cc5eba9157b6e65d6b87647dad4018ff3c84ef
ujjwal <ujjwalg21@iitk.ac.in>
2022-08-24 18:09:13 +0530

    added all files

---
+++ src/MyComponents/ContactUs.js
@@ -0,0 +1,155 @@
+import React from "react";
+
+export default function Contactus() {
+    
+
+    return (
+    <>
+
+        
+        <div className="container text-center mt-5 mb-2">
+        
+        
+        </div>
+        <div className="container mt-3 "  >
+        <div className="row">
+        <span className="managers text-center" >
+            Zonal Managers
+        </span>
+        <hr/>
+        <hr/>
+            <div className="col-md-3"  >
+            <div className=" p-3 text-center rounded box contentbox">
+                <img
+                className="img-responsive rounded-circle dp"
+                src="https://i.imgur.com/uppKNuF.jpg"
+                width="200"
+                />
+                <h5 className="mt-3 name">Samuel Plamer</h5>
+                
+                <i className="fa-solid fa-phone"> +91 78229371701</i>
+                <div className="mt-4">
+                <div>
+                    <a href="https://www.facebook.com/"><img className="logo" src="https://th.bing.com/th/id/R.f1a9464b88fd0014fb9ec7b8c986cfa4?rik=%2b4ZEvogozHErbA&riu=http%3a%2f%2fwww.geekosystem.com%2fwp-content%2fuploads%2f2014%2f01%2ffacebook-logo-480x480.png&ehk=6nSH0IvyhSUtw3VGCfwe5B%2bseRXHaT66WX9W1ZdBcuw%3d&risl=&pid=ImgRaw&r=0" width="45px"/></a>
+                    <a href="https://www.instagram.com/"><img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png" width="45px"/></a>
+                    <a href="https://www.linkedin.com/"><img className="logo" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="45px"/></a>
+                </div>
+
+                </div>
+            </div>
+            </div>
+            <div className="col-md-3">
+            <div className="p-3 text-center rounded box contentbox">
+                <img
+                className="img-responsive rounded-circle dp"
+                src="https://i.imgur.com/oJmLthK.jpg"
+                width="200"
+                />
+                <h5 className="mt-3 name">Nancy Markus</h5>
+                <i className="fa-solid fa-phone"> +91 78229371701</i>
+            
+                <div className="mt-4">
+                <div >
+                    <a href="https://www.facebook.com/"><img className="logo"src="https://th.bing.com/th/id/R.f1a9464b88fd0014fb9ec7b8c986cfa4?rik=%2b4ZEvogozHErbA&riu=http%3a%2f%2fwww.geekosystem.com%2fwp-content%2fuploads%2f2014%2f01%2ffacebook-logo-480x480.png&ehk=6nSH0IvyhSUtw3VGCfwe5B%2bseRXHaT66WX9W1ZdBcuw%3d&risl=&pid=ImgRaw&r=0" width="45px"/></a>
+                    <a href="https://www.instagram.com/"><img className="logo"src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png" width="45px"/></a>
+                    <a href="https://www.linkedin.com/"><img className="logo"src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="45px"/></a>
+                </div>
+
+                </div>
+            </div>
+            </div>
+            <div className="col-md-3">
+            <div className=" p-3 text-center rounded box contentbox">
+                <img
+                className="img-responsive rounded-circle dp"
+                src="https://i.imgur.com/HFpxxJz.jpg"
+                width="200"
+                />
+                <h5 className="mt-3 name">Martha Col</h5>
+                <i className="fa-solid fa-phone"> +91 78229371701</i>
+                <div className="mt-4">
+                <div>
+                    <a href="https://www.facebook.com/"><img className="logo" src="https://th.bing.com/th/id/R.f1a9464b88fd0014fb9ec7b8c986cfa4?rik=%2b4ZEvogozHErbA&riu=http%3a%2f%2fwww.geekosystem.com%2fwp-content%2fuploads%2f2014%2f01%2ffacebook-logo-480x480.png&ehk=6nSH0IvyhSUtw3VGCfwe5B%2bseRXHaT66WX9W1ZdBcuw%3d&risl=&pid=ImgRaw&r=0" width="45px"/></a>
+                    <a href="https://www.instagram.com/"><img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png" width="45px"/></a>
+                    <a href="https://www.linkedin.com/"><img className="logo" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="45px"/></a>
+                </div>
+
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+
+        <hr/>
+        
+
+        <div className="container mt-3 "  >
+        <div className="row">
+        <span className="text-center managers">
+            Head, Events
+        </span>
+        <hr/>
+        <hr/>
+            <div className="col-md-3">
+            <div className=" p-3 text-center rounded box contentbox">
+                <img
+                className="img-responsive rounded-circle dp"
+                src="https://i.imgur.com/MZm1LNz.jpg"
+                width="200"
+                />
+                <h5 className="mt-3 name">Marry Jones</h5>
+                <i className="fa-solid fa-phone"> +91 78229371701</i>
+                <div className="mt-4">
+                <div>
+                    <a href="https://www.facebook.com/"><img className="logo" src="https://th.bing.com/th/id/R.f1a9464b88fd0014fb9ec7b8c986cfa4?rik=%2b4ZEvogozHErbA&riu=http%3a%2f%2fwww.geekosystem.com%2fwp-content%2fuploads%2f2014%2f01%2ffacebook-logo-480x480.png&ehk=6nSH0IvyhSUtw3VGCfwe5B%2bseRXHaT66WX9W1ZdBcuw%3d&risl=&pid=ImgRaw&r=0" width="45px"/></a>
+                    <a href="https://www.instagram.com/"><img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png" width="45px"/></a>
+                    <a href="https://www.linkedin.com/"><img className="logo" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="45px"/></a>
+                </div>
+
+    
+                </div>
+            </div>
+            </div>
+            <div className="col-md-3 "  >
+            <div className=" p-3 text-center rounded box contentbox">
+                <img
+                className="img-responsive rounded-circle dp"
+                src="https://i.imgur.com/uppKNuF.jpg"
+                width="200"
+                />
+                <h5 className="mt-3 name">Samuel Plamer</h5>
+                <i className="fa-solid fa-phone"> +91 78229371701</i>
+                <div className="mt-4">
+                <div>
+                    <a href="https://www.facebook.com/"><img className="logo" src="https://th.bing.com/th/id/R.f1a9464b88fd0014fb9ec7b8c986cfa4?rik=%2b4ZEvogozHErbA&riu=http%3a%2f%2fwww.geekosystem.com%2fwp-content%2fuploads%2f2014%2f01%2ffacebook-logo-480x480.png&ehk=6nSH0IvyhSUtw3VGCfwe5B%2bseRXHaT66WX9W1ZdBcuw%3d&risl=&pid=ImgRaw&r=0" width="45px"/></a>
+                    <a href="https://www.instagram.com/"><img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png" width="45px"/></a>
+                    <a href="https://www.linkedin.com/"><img className="logo" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="45px"/></a>
+                </div>
+
+                </div>
+            </div>
+            </div>
+            <div className="col-md-3 "  >
+            <div className=" p-3 text-center rounded box contentbox">
+                <img
+                className="img-responsive rounded-circle dp"
+                src="https://i.imgur.com/uppKNuF.jpg"
+                width="200"
+                />
+                <h5 className="mt-3 name">Samuel Plamer</h5>
+                <i className="fa-solid fa-phone"> +91 78229371701</i>
+                <div className="mt-4">
+                <div>
+                    <a href="https://www.facebook.com/"><img className="logo" src="https://th.bing.com/th/id/R.f1a9464b88fd0014fb9ec7b8c986cfa4?rik=%2b4ZEvogozHErbA&riu=http%3a%2f%2fwww.geekosystem.com%2fwp-content%2fuploads%2f2014%2f01%2ffacebook-logo-480x480.png&ehk=6nSH0IvyhSUtw3VGCfwe5B%2bseRXHaT66WX9W1ZdBcuw%3d&risl=&pid=ImgRaw&r=0" width="45px" /></a>
+                    <a href="https://www.instagram.com/"><img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png" width="45px"/></a>
+                    <a href="https://www.linkedin.com/"><img className="logo" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="45px"/></a>
+                </div>
+
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </>
+    );
+}
